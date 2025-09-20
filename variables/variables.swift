0cefa93d03a72a54769632fbda0462f4f2cef5f4
PeterKimLiz <pkironji@yahoo.com>
2025-09-20 20:49:11 +0300

    variable

---
+++ variables/variables.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+// declaring variable can be done into two ways in swift using var or let
+//var is mutable while let is immutable 
+var myname = "Peter";
+myname = "Peter kim"
+
+let myAge = 30;
+//myAge=56 will throw an error since you cannnot modify  the value of let once assigned 
+let date = Date()
+
+print("\(myname) you are \(myAge) years old as of \(date)")
+
+let userType:String
+
+if myAge >= 18 {
+    userType = "you are an adult"
+}else {
+    userType = "you are a minor"
+}
+
+print("Hello \(myname) \(userType) ")
